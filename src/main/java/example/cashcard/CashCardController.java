2b86ab17f1477656a8a14c030ef265217649ad5c
Mikael92002 <Mikael92002@users.noreply.github.com>
2025-02-02 18:23:50 -0600

    Refactored code by implementing helper method findCashCard. The method is used in both putCashCard and findById.

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -35,7 +35,7 @@ class CashCardController {
         } else {
             return ResponseEntity.notFound().build();
         }*/
-        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard cashCard = findCashCard(requestedId, principal);
         if(cashCard != null){
             return ResponseEntity.ok(cashCard);
         }
@@ -63,12 +63,16 @@ class CashCardController {
 
     @PutMapping("/{requestedId}")
     private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
-        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard cashCard = findCashCard(requestedId, principal);
         if (cashCard != null) {
             CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
             cashCardRepository.save(updatedCashCard);
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    private CashCard findCashCard(Long requestedId, Principal principal){
+        return cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
     }
 }
